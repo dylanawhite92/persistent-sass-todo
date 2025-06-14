f8376946928221dfec50f316871d79d5979ab75b
The Crystal King <dylanawhite11@gmail.com>
2025-06-14 15:37:04 -0500

    framed out procedural steps for list

--- js/main.js
+++ js/main.js
@@ -3,3 +3,26 @@ import ToDoItem from './todoitem';
 import ToDoList from './todolist';
 
 const ToDoList = new ToDoList();
+
+// Launch app
+document.addEventListener('readystatechange', (e) => {
+  if (e.target.readyState === 'complete') {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  // Add listeners
+
+  // Procedural
+  // Load list object
+
+  refreshThePage();
+};
+
+const refreshThePage = () => {
+  clearListDisplay();
+  // renderList();
+  // clearItemEntryField();
+  // setFocusOnItemEntryField();
+};
